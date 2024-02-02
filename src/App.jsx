bc61a02d3a09ee4dc67aa4cc7e91b79454edeaf5
amir nabiizade <amirnabiizade@gmail.com>
2024-02-02 16:37:59 +0330

    added reacr hot toast and added toasts and make main better with some css

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Bookings from "./pages/Bookings";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,28 +25,44 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-        <>
-    <QueryClientProvider client={queryClient}>
-          <GlobalStyles />
-          <BrowserRouter>
-            <Routes>
-              {/* <Route index element={<Dashboard />} /> */}
-              <Route element={<AppLayout />}>
-                <Route index element={<Navigate replace to="dashboard" />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="bookings" element={<Bookings />} />
-                <Route path="cabins" element={<Cabins />} />
-                <Route path="users" element={<Users />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="account" element={<Account />} />
-              </Route>
-              <Route path="login" element={<Login />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </BrowserRouter>
-          <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-        </>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            {/* <Route index element={<Dashboard />} /> */}
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="cabins" element={<Cabins />} />
+              <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="account" element={<Account />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Toaster position="top-center" gutter={12} containerStyle={{margin:"8px"}} toastOptions={{
+          success:{
+            duration: 3000,
+          },
+          error: {
+            duration: 5000
+          },
+          style:{
+            fontSize:"16px",
+            maxWidth:"500px",
+            padding: "16px 24px",
+            backgroundColor: 'var(--color-grey-0)',
+            color: 'var(--color-grey-700)'
+          }
+
+        }} />
+      </QueryClientProvider>
+    </>
   );
 }
 
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -54,12 +55,12 @@ function CabinRow({ cabin }) {
   const { isPending: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert("cabin successfully deleted");
+      toast.success("cabin successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.err(err.message),
   });
   return (
     <TableRow role="row">
