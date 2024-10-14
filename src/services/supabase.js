5bfd1e5df16e35cf4109bad85b7105b21506e418
amirnabiizade <amirnabiizade@gmail.com>
2024-10-14 10:15:59 +0330

    update supa base url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://jxtlrihhiwnpzaecsaeh.supabase.co";
+export const supabaseUrl = "https://axzuplqutyqylzrxrela.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4enVwbHF1dHlxeWx6cnhyZWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzNzQ5NzQsImV4cCI6MjA0Mzk1MDk3NH0.Moa15doBQiY7oafbDUZA9QuKD0ZrvhIw0jWed2JFf1k";
 const supabase = createClient(supabaseUrl, supabaseKey);
